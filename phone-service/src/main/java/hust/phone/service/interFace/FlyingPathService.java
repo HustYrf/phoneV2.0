c1031ffd29bcd53f3fc558c44b570fac76f0a48b
hp <hp@windows10.microdone.cn>
2018-11-30 14:13:34 +0800

    初步完成接口功能

--- phone-service/src/main/java/hust/phone/service/interFace/FlyingPathService.java
+++ phone-service/src/main/java/hust/phone/service/interFace/FlyingPathService.java
@@ -3,12 +3,14 @@ package hust.phone.service.interFace;
 import java.util.List;
 
 import hust.phone.mapper.pojo.FlyingPath;
+import hust.phone.utils.pojo.PlanePathVo;
 
 public interface FlyingPathService {
 
     boolean insertPlanePath(FlyingPath flyingPath);
 
     FlyingPath selectByPlanepathId(FlyingPath flyingPath);
+    List<PlanePathVo> getPathToObject(int pathId);
 
 	/*List<FlyingPath> findAllplanePath();*/
 }
